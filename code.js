acedec4d9ec9b40abaf97ae85d9b5b6cf951f3c4
Roman Tarnavski <roman@romant.net>
2015-04-04 15:45:20 +1100

    simplifying to one

--- code.js
+++ code.js
@@ -2,16 +2,11 @@ var app = angular.module('myApp', ['frapontillo.bootstrap-switch']);
 
 app.controller('bootswitch-controller', function ($scope)
 {
-    $scope.$watch('isSelectedExample1', function(newValue, oldValue)
-    {
-      console.log('Example 1 - old:' + oldValue + ": Example 1 -  new :" + newValue);
-    });
-    
     //want this one to be 'on' by default
-    $scope.selectionCriteria2 = true;
+    $scope.selectionCriteria = true;
 
-    $scope.$watch('selectionCriteria2', function(newValue, oldValue)
+    $scope.$watch('selectionCriteria', function(newValue, oldValue)
     {
-      console.log('Example 2 - old:' + oldValue + ": Example 2 -  new :" + newValue);
+      console.log('Example - old:' + oldValue + ": Example -  new :" + newValue);
     });   
 });
